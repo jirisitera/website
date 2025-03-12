47c2d66355639a72efdf5a7efe9c685f4560ceaa
jirisitera <jirik.sitera@gmail.com>
2025-03-13 00:13:24 +0100

    fix github pages errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   output: "static",
-  base: '/',
+  site: 'https://jirisitera.github.io',
+  base: '/website',
   vite: {plugins: [tailwindcss()]},
 });
