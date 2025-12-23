8df1c1a391dbd7e0cbb1468ee370a17866437dc1
jirisitera <78226565+jirisitera@users.noreply.github.com>
2025-12-23 04:22:24 +0100

    init: add functional website base

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from "astro/config"
+import tailwindcss from "@tailwindcss/vite"
+export default defineConfig({
+    output: "static",
+    site: "https://japicraft.com",
+    vite: { plugins: [tailwindcss()] }
+})
